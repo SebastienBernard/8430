9366d26a39076a67c03ce30c7ed522985b781348
Tristan Beauregard-Choquette <tbchoquette@hotmail.com>
2018-03-20 18:47:50 -0400

    Commit yolo: combinaison de l'exemple authentification de l'API Spotify dans notre app.js + intégration partielle de fonction de recherche

---
+++ server/SpotifyTools.js
@@ -0,0 +1,50 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+var fs = require('fs');
+
+module.exports = {
+	
+	
+	generateRandomString: function(length) {
+	  var text = '';
+	  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+	  for (var i = 0; i < length; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	  }
+	  return text;
+	};
+	
+	var searchterm;
+	
+	SongSearch: function (searchterm) {
+
+		var spotifyApi = new SpotifyWebApi({
+		  clientId : 'ab85096602544cd1a072309a632747ff',
+		  clientSecret : 'a6338157c9bb5ac9c71924cb2940e1a7',
+		  redirectUri : 'http://localhost:8888/callback'
+		});
+
+		spotifyApi.setAccessToken('BQCCLjCac3S3I--16bv8gIADoFlX3h_BfVa2_JAMIXdPKUL2rM0o7iEuVUvIhtWwbFmc3s3lD70GRxPB5FR1iqd7AjISwdgODFDgOCpE7ya3_8mHtbqxr_jGz4UVYbUHthQi0hlppRAIOYBsAxwWFJHkE88SBAj4-v5RhaALwNrv3bt3DlI0C1EN8S3n2kqDAxGL_ZmOJfpNHtFW6-nhytHBvIninTXsTyTcmF6HGm8');
+		  
+		var PlaylistTestID = 'string';  
+		  
+		  //Dans le serveur: prendre a capture les inputs de la bar et remplacer dans le field de spotifyApi.searchTracks()
+		  //Probablement avec document.getElementById('xxxxxx_ID').value
+		  var SearchArr = new Array();
+		  spotifyApi.searchTracks(searchterm);
+		  .then(function(data) {
+					for(i in data.body.tracks.items){
+						SearchArr.push({
+								name: data.body.tracks.items[i].name,
+								uri: data.body.tracks.items[i].uri,
+								supplier: 'Spotify'
+							})
+					}
+					
+					
+		  }, function(err) {
+			console.log('Something went wrong!', err);
+		  });
+
+	}
+}
--- server/index.js
+++ server/index.js
@@ -5,6 +5,17 @@ const numCPUs = require('os').cpus().length;
 
 const PORT = process.env.PORT || 5000;
 
+var request = require('request'); // "Request" library
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+var fs = require('fs');
+
+var SearchTool = require('./SpotifyTools');
+
+var client_id = 'ab85096602544cd1a072309a632747ff'; // Your client id
+var client_secret = 'c967f17bd14341619864cc17b898a908'; // Your secret
+var redirect_uri = 'https://obscure-island-46206.herokuapp.com/callback'; // Your redirect uri
+
 // Multi-process to utilize all CPU cores.
 if (cluster.isMaster) {
   console.error(`Node cluster master ${process.pid} is running`);
@@ -30,6 +41,126 @@ if (cluster.isMaster) {
     var answer = JSON.stringify([{Name:"bonjour",Supplier:"spotify"},{Name:"bonjour",Supplier:"spotify"}]);
     res.send(answer);
   });
+
+
+var stateKey = 'spotify_auth_state';
+
+app.use(express.static(__dirname + '/public'))
+   .use(cookieParser());
+
+   //LOGIN
+app.get('/login', function(req, res) {
+
+  var state = SpotifyTools.generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  //requests authorization
+  var scope = 'user-read-private user-read-email playlist-read-private playlist-modify-private playlist-modify-public playlist-read-collaborative streaming';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+
+app.get('/callback', function(req, res) {
+
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+          console.log(body);
+		  	fs.writeFile('AccessTokenSpotify.txt', access_token, function (err) {
+			if (err) throw err;
+			console.log('Saved!');
+			});
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/#' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
+
+app.get('/refresh_token', function(req, res) {
+
+  // requesting access token from refresh token
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.send({
+        'access_token': access_token
+      });
+    }
+  });
+});
+
+app.get('/search', function(req, res {
+	res.set('Content-Type', 'application/json');
+    var answer = JSON.stringify([{Name:"bonjour",Supplier:"spotify"},{Name:"bonjour",Supplier:"spotify"}]);
+    res.send(answer);
+}
 
   app.listen(PORT, function () {
     console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
